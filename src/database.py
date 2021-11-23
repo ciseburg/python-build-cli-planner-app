15fa6c5e91d7a10cebcea34dbe69662692287039
ciseburg <iseburg@gmail.com>
2021-11-22 23:33:00 -0600

    Task 9 - Accept conforming reminder class

--- src/database.py
+++ src/database.py
@@ -1,6 +1,7 @@
 import csv
-from src.reminder import PoliteReminder
-from src.deadlined_reminders import DateReminder
+# from src.reminder import PoliteReminder
+# from src.deadlined_reminders import DateReminder
+from src.deadlined_reminders import DeadlinedReminder
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -14,10 +15,13 @@ def list_reminders():
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text, date):
+def add_reminder(text, date, ReminderClass):
 
     # reminder = PoliteReminder(text=text)
-    reminder = DateReminder(text=text, date=date)
+    if not issubclass(ReminderClass, DeadlinedReminder):
+        raise TypeError("Invalid Reminder Class")
+
+    reminder = ReminderClass(text=text, date=date)
    
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
