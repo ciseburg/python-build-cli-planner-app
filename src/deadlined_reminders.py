f50ae3d3558bdb0bce4664677a896ebce3118092
ciseburg <iseburg@gmail.com>
2021-11-22 20:41:08 -0600

    DateReminder Class - Tasks 5,6 & 7

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -1,6 +1,9 @@
 from abc import ABCMeta, abstractmethod
 from abc import ABC
 from collections.abc import Iterable
+from datetime import datetime
+from dateutil.parser import parse
+from datetime import datetime
 
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
 
@@ -13,3 +16,19 @@ class DeadlinedReminder(Iterable, ABC):
     @abstractmethod
     def is_due(self):
         pass
+
+#Derived class
+class DateReminder(DeadlinedReminder):
+    def __init(self, text, date):
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+
+    # Overriding base class
+    def is_due(self):
+        # return super().is_due()
+        self.date <= datetime.now()
+    
+    def __iter__(self):
+        text = self.text
+        formatted_date = self.date.isoformat()
+        return iter(text, formatted_date)
