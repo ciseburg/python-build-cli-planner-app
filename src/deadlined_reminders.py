a548db27d97ac98eaffe4297b8a6047efc6aa45b
ciseburg <iseburg@gmail.com>
2021-11-22 19:55:15 -0600

    Adding self paramerter to method

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -5,11 +5,11 @@ from collections.abc import Iterable
 class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
 
     @abstractmethod
-    def is_due():
+    def is_due(self):
         pass
 
 class DeadlinedReminder(Iterable, ABC):
 
     @abstractmethod
-    def is_due():
+    def is_due(self):
         pass
